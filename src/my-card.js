346e3300d8d6bf2ba234662249b795296e542615
emilygaddis <eagadds@gmail.com>
2024-02-11 23:51:34 -0500

    Added Button Functionality

--- src/my-card.js
+++ src/my-card.js
@@ -1,10 +1,5 @@
 import { LitElement, html, css } from 'lit';
 
-/**
- * Now it's your turn. Here's what we need to try and do
- * 1. 
- */
-
 export class MyCard extends LitElement {
 
   static get tag() {
@@ -17,19 +12,59 @@ export class MyCard extends LitElement {
     this.imageurl = "#";
     this.description = "Details";
     this.buttonlink = "#";
+    this.fancy = false;
   }
 
   static get styles() {
     return css`
       :host {
         display: block;
+        background-color: lightblue;
+        padding: 20px;
+        margin: 12px;
+        width: 350px;
       }
 
-      button.btn:hover {
-        background-color: blue;
-        color: white;
+      :host([fancy]) {
+      display: block;
+        background-color: pink;
+        border: 2px solid fuchsia;
+        box-shadow: 10px 5px 5px red;
+      }
+
+      .change-color{
+        background-color: #ff9700;
+      }
+
+      details summary {
+        text-align: left;
+        font-size: 20px;
+        padding: 8px;
+      }
+
+      details[open] summary {
+        font-weight: bold;
+      }
+  
+      details div {
+        border: 2px solid black;
+        text-align: left;
+        padding: 8px;
+        height: 70px;
+        overflow: auto;
       }
     `;
+  }
+
+  // put this anywhere on the MyCard class; just above render() is probably good
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
   }
 
   render() {
@@ -37,22 +72,25 @@ export class MyCard extends LitElement {
     return html`
 
     <div class = "card">
-      <h1 class = "heading">${this.title}</h1>
       <img class = "image" src="${this.imageurl}" style="width: 300px; object-fit: scale-down;">  
-      <p class = "paragraph">${this.description}</p>
-      <a href="${this.buttonlink}" rel="noopener noreferrer">
-        <button class="btn">Details</button>
-      </a>
+      <h2 class = "heading">${this.title}</h2>
+      <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+        <summary>Description</summary>
+        <div>
+          <slot>${this.description}</slot>
+        </div>
+      </details>
     </div>
     `;
   }
 
   static get properties() {
-    return {
+    return {  
       title: { type: String },
       imageurl: { type: String },
       description: { type: String },
       buttonlink: { type: String },
+      fancy: { type: Boolean, reflect: true },
     };
   }
 }
